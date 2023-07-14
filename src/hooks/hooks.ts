c308191f9ada472d687829b76bb0213b335d5fad
SUYCOKHWLAP300\Suyati <agopalakrishnan@suyati.com>
2023-07-14 12:54:39 +0530

    Email configuration updated

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -97,41 +97,4 @@ function getStorageState(user: string): string | { cookies: { name: string; valu
     else if (user.endsWith("lead"))
         return "src/helper/auth/lead.json";
 
-    const nodemailer = require("nodemailer");
-
-    async function emailSend(error) {
-        try {
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.office365.com',
-                port: 587,
-                secure: true,
-                auth: {
-                    user: 'akhil.krishnan@interfacesys.com',
-                    pass: 'Interface5',
-                },
-            });
-
-            let message = {
-                from: 'akhil.krishnan@interfacesys.com',
-                to: 'agopalakrishnan@suyati.com',
-                subject: 'Testing Subject',
-                text: error,
-                attachments: [
-                    {
-                        filename: 'index.html',
-                        path: 'test-results/report/',
-                    },
-                ],
-            };
-
-            let info = await transporter.sendMail(message);
-            console.log('=====Email Sent=====');
-        } catch (error) {
-            throw new Error(error);
-        }
-    }
-
-    emailSend("Example error message");
-
-
 }
